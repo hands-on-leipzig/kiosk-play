cb68a0028d5a0613f45ec53d516fe00036716844
Lucas Welscher <ga53foy@mytum.de>
2025-03-14 09:10:42 +0100

    add some testimages

--- fe/src/components/Carousel.vue
+++ fe/src/components/Carousel.vue
@@ -3,9 +3,11 @@ import '@splidejs/vue-splide/css';
 import {ref} from "vue";
 import {Slide} from "../model/slide.js";
 import SlideContentRenderer from "./slides/SlideContentRenderer.vue";
+import {ImageSlideContent} from "../model/imageSlideContent.js";
 
 const slides = ref([
-  new Slide(1, 'Testbild'),
+  new Slide(1, 'Testbild', new ImageSlideContent('https://www.first-lego-league.org/files/relaunch2022/theme/layout/fll/logo/vertical/FIRSTLego_IconVert_RGB.png')),
+  new Slide(1, 'Testbild', new ImageSlideContent('https://www.first-lego-league.org/assets/images/f/FLL_C_Robi_Detail_Girl_BG-3aaabbd0.webp')),
 ]);
 </script>
 

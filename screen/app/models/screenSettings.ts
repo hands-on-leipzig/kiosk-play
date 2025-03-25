f472bc56f66a1f3ecbfcb8b78a0559da997a5caa
Lucas Welscher <ga53foy@mytum.de>
2025-03-25 19:17:37 +0100

    fix background image

--- screen/app/models/screenSettings.ts
+++ screen/app/models/screenSettings.ts
@@ -14,4 +14,4 @@ export class ScreenSettings {
     }
 }
 
-export const dachScreenSettings = new ScreenSettings(0, false, [], '/images/Hintergrund.png', 8);
+export const dachScreenSettings = new ScreenSettings(0, false, [], '/be/images/Hintergrund.png', 8);

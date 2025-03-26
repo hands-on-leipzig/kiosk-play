cd999b7389072b5a3c615ae930e6c43043f657c0
Lucas Welscher <ga53foy@mytum.de>
2025-03-26 12:36:07 +0100

    fix footer

--- screen/app/components/Footer.tsx
+++ screen/app/components/Footer.tsx
@@ -9,7 +9,7 @@ interface FooterProps {
 const Footer: React.FC<FooterProps> = ({ settings }) => {
     return (
         settings?.showFooter && (
-            <footer className="bg-white flex justify-evenly items-center" style={{ height: '15vh', position: 'absolute', bottom: 0 }} id="screenFooter">
+            <footer className="bg-white w-full flex justify-evenly items-center" style={{ height: '15vh', position: 'absolute', bottom: 0 }} id="screenFooter">
                 {settings?.footerImages.map((image) => (
                     <div key={image} className="flex px-8">
                         <img src={image} alt="" style={{ maxHeight: '13vh' }} />

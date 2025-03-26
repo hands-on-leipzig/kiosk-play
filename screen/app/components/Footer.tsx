4997304c69b23a704d9cbee48f51b04ff915a199
Lucas Welscher <ga53foy@mytum.de>
2025-03-26 10:54:00 +0100

    resolve logo url

--- screen/app/components/Footer.tsx
+++ screen/app/components/Footer.tsx
@@ -6,15 +6,13 @@ interface FooterProps {
     settings: ScreenSettings | null;
 }
 
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
-
 const Footer: React.FC<FooterProps> = ({ settings }) => {
     return (
         settings?.showFooter && (
             <footer className="bg-white flex justify-evenly items-center" style={{ height: '15vh', position: 'absolute', bottom: 0 }} id="screenFooter">
                 {settings?.footerImages.map((image) => (
                     <div key={image} className="flex px-8">
-                        <img src={BASE_URL + image} alt="" style={{ maxHeight: '13vh' }} />
+                        <img src={image} alt="" style={{ maxHeight: '13vh' }} />
                     </div>
                 ))}
             </footer>

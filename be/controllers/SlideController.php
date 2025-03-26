0a42e39b8bc746912e1f7f3e76f14cfff95ff013
Jakub Stříbrný <35335130+kubajznik@users.noreply.github.com>
2025-03-26 20:01:47 +0100

    added slide controller

--- be/controllers/SlideController.php
+++ be/controllers/SlideController.php
@@ -4,21 +4,27 @@ namespace controllers;
 
 class SlideController extends Controller
 {
-public function addSlide()
-{
-    $slide = [];
-    foreach ($_POST as $param => $value) {
-        $value = mysqli_real_escape_string($this->db->mysqli, $value);
-        $slide[$param] = $value;
+    public function addSlide()
+    {
+        $slide = [];
+        foreach ($_POST as $param => $value) {
+            $value = mysqli_real_escape_string($this->db->mysqli, $value);
+            $slide[$param] = $value;
+        }
+        $screen = 1;
+
+        mysqli_stmt_prepare($this->db->stmt, "INSERT INTO slides (screen, title, content) VALUES (?,?,?)");
+        mysqli_stmt_bind_param($this->db->stmt, "iss", $screen, $slide["title"], $slide["content"]);
+        try {
+            mysqli_stmt_execute($this->db->stmt);
+        } catch (\Exception $e) {
+            throw $e;
+        }
+        return null;
     }
 
-    mysqli_stmt_prepare($this->db->stmt,"INSERT INTO slides (screen, title, content) VALUES (?,?,?)");
-    mysqli_stmt_bind_param($this->db->stmt, "sss", $slide);
-    try {
-        mysqli_stmt_execute($this->db->stmt);
-    } catch (\Exception $e) {
-        throw $e;
+    public function fetchSlides(): string
+    {
+        return json_encode($this->db->select("SELECT * FROM slides WHERE id = " . $this->event_id));
     }
-    return null;
-}
 }

a0d02151e84ca5f18c0394492d280b74804d2fcf
Lucas Welscher <ga53foy@mytum.de>
2025-04-01 10:41:10 +0200

    remove react screen

--- screen/app/models/score.ts
+++
@@ -1,9 +0,0 @@
-export class Score {
-    points: number;
-    highlight: boolean;
-
-    constructor(points: number, highlight: boolean) {
-        this.points = points;
-        this.highlight = highlight;
-    }
-}
